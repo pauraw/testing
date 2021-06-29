da74682c6e923acfd3c76e4185258777db35284b
Paul <33725364+pauraw@users.noreply.github.com>
2021-06-29 11:17:56 +0200

    Create config-gallery.js

---
+++ config-gallery.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  
+  var entries = window.PluginManager.getPluginList().GallerySlider;
+  
+  if (entries !== "undefined") {
+    entries.get("instances")[0]._sliderSettings.preventScrollOnTouch = false;
+  }
+  
+});
